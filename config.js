8fbbb4e716ab07d54bd71f6007cf7335de4e3ac0
Winstone <70216345+WinstoneDev@users.noreply.github.com>
2020-08-29 20:09:25 +0200

    Add config.js 🎇

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  token: "", // Your bot's token
+  prefix: "x!", // Any prefix
+  ownerID: "", // Your ID
+  emojis: {
+  false: "<:false:744117500119351297>",
+  signal: "<:signal:744123219572686869>",
+  true: "<:check:744117593128042606>",
+  refresh: "<:refresh:744122805087502407>",
+  links: '<:links:749307669004943482>',
+  crown: '<:owner:749308502694166610>',
+  coins: '<:coins:744122717745446942>',
+  money: '<:money:744123833958662174>',
+  profilem: '<:profilem:744124316148432926>',
+  profilef: "<:profilef:744124471685808198>",
+  idcard: '<:idcard:744124009062334526>',
+  server: '<:server:744123145195094046>', 
+  lock: '<:close:744123062382887004>',
+  clock : '<:heure:744122982745505862>',
+  search: '<:search:744122879813222441>',
+  online: '<:online:744117789505093673>',
+  idle: '<:idle:744117901912703037>',
+  dnd: '<:dnd:744117855808782417>',
+  daily: '<:salary:744125185573257246>',
+  staff : '<:staff:749310628430086285>',
+  },
+  pg_user: "", // Your PostgreSQL User
+  pg_pass: "", // Your PostgreSQL User's password
+  pg_db: "", // The name of the database that you want to name. Make sure to run /database/generate-db.js before starting the bot
