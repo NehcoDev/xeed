0a1341f5820ddcb13d66ae7f81b8ff24d27bb6e8
Winstone <70216345+WinstoneDev@users.noreply.github.com>
2020-08-29 20:50:31 +0200

    Delete sharder.js ⛔

--- sharder.js
+++
@@ -1,9 +0,0 @@
-const {
-    ShardingManager
-} = require('discord.js');
-const config = require("./config.js")
-const manager = new ShardingManager('./index.js', {
-    token: config.token
-});
-
-manager.spawn(2);
